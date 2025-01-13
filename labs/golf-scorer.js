782060955bdabd9f382ea86d135d83f3d2ddd7ba
Chris L <bobalua@gmail.com>
2025-01-12 21:16:10 -0500

    golf-scorer lab complete

--- labs/golf-scorer.js
+++ labs/golf-scorer.js
@@ -27,6 +27,34 @@ export const testCases = [
         input: [ 7, 4 ],
         output: 'Go Home!',
     },
+    {
+    input: [ 1, Math.random() * 10 ],
+        output: 'Hole-in-one!',
+    },
+    {
+        input: [ 98, 100 ],
+        output: 'Eagle',
+    },
+    {
+        input: [ 99, 100 ],
+        output: 'Birdie',
+    },
+    {
+        input: [ 100, 100 ],
+        output: 'Par',
+    },
+    {
+        input: [ 101, 100 ],
+        output: 'Bogey',
+    },
+    {
+        input: [ 102, 100 ],
+        output: 'Double Bogey',
+    },
+    {
+        input: [ 103, 100 ],
+        output: 'Go Home!',
+    }
 ];
 
 /* Golf Scorer
@@ -36,6 +64,19 @@ ect.
 */
 
 export function subject(swings, par) {
-    return 'Par';
+    if (swings == 1) {
+        return 'Hole-in-one!';
+    }else if (swings - par == -2) {
+        return 'Eagle';
+    }else if (swings - par == -1) {
+        return 'Birdie';
+    }else if (swings == par) {
+        return 'Par';
+    }else if (swings - par == 1) {
+        return 'Bogey';
+    }else if (swings - par == 2) {
+        return 'Double Bogey';
+    }else if (swings - par >= 3) {
+        return 'Go Home!'
+    };
 };
-
