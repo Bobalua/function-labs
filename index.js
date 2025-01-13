eb4418bf3ae840c86dfd2b0da13b3bdf1987390d
zinefer <zinefer@gmail.com>
2025-01-12 18:48:32 -0700

    Improve index.js

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
+// This is called like:
+// $ node index.js golf-scorer
+// Which will execute the subject function in labs/golf-scorer.js
+// against the testCases in that file.
+// Then a result will be printed to the console.
 
-async function executeFunctionLab(filePath, testCases) {
-  const { testCases, subject } = await import(path.join(dirname, 'labs', filePath));
+const filename = process.argv[2];
+
+async function executeFunctionLab() {
+  const { testCases, subject } = await import(`./labs/${filename}.js`);
   const results = [];
   testCases.forEach((testCase, index) => {
     const result = subject(...testCase.input);
 
     if (result === testCase.output) {
-      results.push('Pass');
+      results.push(`\x1b[42m[PASS]\x1b[0m`);
     } else {
-      results.push('Fail');
+      results.push(`\x1b[41m[FAIL]\x1b[0m`);
     }
   });
+  
+  // And color code the results.
+  console.log(`labs/${filename}.js:`);
+  results.forEach((result, index) => {
+    console.log(`  ${result}`);
+  });
 }
+
+executeFunctionLab();
