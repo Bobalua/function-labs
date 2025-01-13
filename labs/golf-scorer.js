fe1b7a68ab9aba5b7e94f8814d13e9a26823a8f1
zinefer <zinefer@gmail.com>
2025-01-12 18:36:18 -0700

    Init golf-scorer

--- labs/golf-scorer.js
+++ labs/golf-scorer.js
@@ -1,9 +1,39 @@
 export const testCases = [
     {
+        input: [ 1, 4 ],
+        output: 'Hole-in-one!',
+    },
+    {
+        input: [ 2, 4 ],
+        output: 'Eagle',
+    },
+    {
+        input: [ 3, 4 ],
+        output: 'Birdie',
+    },
+    {
         input: [ 4, 4 ],
         output: 'Par',
-    }
+    },
+    {
+        input: [ 5, 4 ],
+        output: 'Bogey',
+    },
+    {
+        input: [ 6, 4 ],
+        output: 'Double Bogey',
+    },
+    {
+        input: [ 7, 4 ],
+        output: 'Go Home!',
+    },
 ];
+
+/* Golf Scorer
+Return the result of a golf game based on the number of swings and the par.
+1 swing is a "Hole-in-one!"
+ect.
+*/
 
 export function subject(swings, par) {
     return 'Par';
