6007cb04c60ac028c05633e07bd7d96659586617
zinefer <zinefer@gmail.com>
2025-01-28 12:16:58 -0700

    Init sum-finder lab

---
+++ labs/sum-finder.js
@@ -0,0 +1,33 @@
+const testCases = [
+    {
+        input: [[2,7,11,15], 9],
+        output: [0,1], // 2 + 7 = 9
+    },
+    {
+        input: [[3,2,4], 6],
+        output: [1,2], // 2 + 4 = 6
+    },
+    {
+        input: [[3,3], 6],
+        output: [0,1], // 3 + 3 = 6
+    },
+]
+
+/*
+    Sum Finder
+
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        - You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        - Sort the array before returning the indices.
+
+    Constraints:
+        - Only one valid answer exists.
+
+    Tip: It is often beneficial to brute force the solution first which we can _then_ optimize.
+    Bonus: Can you solve it in less than O(n^2) time complexity?
+*/
+
+export function subject(nums, target) {
+    // Your code here
+    return [];
+}
