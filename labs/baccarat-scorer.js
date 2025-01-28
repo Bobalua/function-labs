fed533c14929d83235b02e6b33278942ba81c9c6
chris lindauer <chrislindauer@LindyPrimeAirM2.attlocal.net>
2025-01-28 05:21:20 -0500

    Baccarat-scorer lab complete, awaiting review

--- labs/baccarat-scorer.js
+++ labs/baccarat-scorer.js
@@ -1,3 +1,5 @@
+// 2025-01-28T10:14:51.625Z - 2 attempts - Success after 2 minutes
+// 2025-01-28T10:12:07.035Z - 4 attempts - Success after 6 minutes
 export const testCases = [
     {
         input: [1, 2],
@@ -31,5 +33,28 @@ Return the total score of a baccarat hand based on the cards.
 */
 
 export function subject(...hand) {
-    return 0;
+    // equating a scoring value for face cards and aces.
+    const faceCardScore  = {
+        'A': 1,
+        'J': 10,
+        'Q': 10,
+        'K': 10
+    };
+    // will check if card is in faceCardScore, if it is, it will use that value for scoring
+    // if not, it will use the numerical value of the card
+    // the third value will do the same, however if it doesn't exist, it will default to a '0'
+    let handScore = ((faceCardScore[hand[0]]) || hand[0]) + ((faceCardScore[hand[1]]) || hand[1]) 
+        + ((faceCardScore[hand[2]]) || hand[2] || 0);  // unsure of moving this to two lines.  please advise on best practices.
+
+    // console.log(handScore); // debug
+
+    // if score is 10 or larger, the value will be converted to a string and the first character will be 'sliced', removing it.
+    // it will then be parsed and re-converted into a usable value.
+    if (handScore > 9) {
+        handScore = JSON.parse(handScore.toString().slice(1));
+        return handScore;
+    // if score is less than 10, its value is already correct and can be returned.
+    } else {
+        return handScore;
+    };
 };
