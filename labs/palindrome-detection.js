f5cd800a212f8b9d09193ba951cb1736bde4f379
zinefer <zinefer@gmail.com>
2025-01-28 12:03:58 -0700

    Init palindrome-detection lab

---
+++ labs/palindrome-detection.js
@@ -0,0 +1,28 @@
+const testCases = [
+    {
+        input: 'racecar',
+        output: true,
+    },
+    {
+        input: 'hello',
+        output: false,
+    },
+    {
+        input: 'tacocat',
+        output: true,
+    },
+    {
+        input: 'taco cat',
+        output: false,
+    },
+    {
+        input: 'madam',
+        output: true,
+    }
+];
+
+/* A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward. */
+
+export function subject(str) {
+    return false;
+}
